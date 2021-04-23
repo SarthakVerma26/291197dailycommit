14e34482327b84467d48ac054676a9a74cb63caf
Sarthak Verma <vermasarthak702@gmail.com>
2021-04-23 11:51:16 +0530

    commit7

---
+++ mergedict.py
@@ -0,0 +1,8 @@
+## initializing the dictionaries
+fruits = {"apple": 2, "orange" : 3, "tangerine": 5}
+dry_fruits = {"cashew": 3, "almond": 4, "pistachio": 6}
+## updating the fruits dictionary
+fruits.update(dry_fruits)
+## printing the fruits dictionary
+## it contains both the key: value pairs
+print(fruits) 
